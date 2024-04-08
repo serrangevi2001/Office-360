495501b101a5fa6f5f383dd2cb4b001bdb3b7e4c
SERRANGEVI <serrangevi@visionwaretechnologies.in>
2024-04-08 14:05:31 +0530

    doc

--- src/modules/Documentation/document.js
+++ src/modules/Documentation/document.js
@@ -10,7 +10,7 @@ const document = () => {
 <h2>Rasipuram Branch.</h2>
 <h3>Total No of Employees : 7 only... </h3>
 <h4>Thank you...</h4>
-<h5>2024</h5>
+<h5>Testing via docker jenkins</h5>
         </CardBody>
     </Card> 
   
