b7f179a63f555b66c4bd8aaffb0f490306f17e1d
SERRANGEVI <serrangevi@visionwaretechnologies.in>
2024-04-10 12:23:04 +0530

    index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 
 const rootElement = document.getElementById('root'); 
@@ -12,11 +12,9 @@ const root = createRoot(rootElement);
 
 root.render(
   <Suspense fallback={<Loader />}>
-    {/* <HashRouter> */}
-      <BrowserRouter>
+    <HashRouter>
       <App />
-      </BrowserRouter>
-    {/* </HashRouter> */}
+    </HashRouter>
   </Suspense>
 );
 
