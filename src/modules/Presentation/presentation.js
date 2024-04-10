0a3ce483b7add11c0e3ed10b4e6f52e33cc1ac88
SERRANGEVI <serrangevi@visionwaretechnologies.in>
2024-04-10 11:17:35 +0530

    presentation

--- src/modules/Presentation/presentation.js
+++ src/modules/Presentation/presentation.js
@@ -6,7 +6,8 @@ const presentation = () => {
     <Card>
         <CardBody>
 <CardTitle>Presentation</CardTitle>
-<h1>It's Presentation</h1>
+<h1>It's Presentation.</h1>
+<h2>welcome</h2>
         </CardBody>
     </Card>
   )
